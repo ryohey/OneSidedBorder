1c8ea404a5f4e896d075ce0597befb0d529a5f40
ryohey <info@codingcafe.jp>
2019-03-25 18:22:21 +0900

    Fix

---
+++ OneSidedBorder/OneSidedBorder.h
@@ -0,0 +1,18 @@
+//
+//  OneSidedBorder.h
+//  OneSidedBorder
+//
+//  Created by ryohey on 2019/03/25.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneSidedBorder.
+FOUNDATION_EXPORT double OneSidedBorderVersionNumber;
+
+//! Project version string for OneSidedBorder.
+FOUNDATION_EXPORT const unsigned char OneSidedBorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneSidedBorder/PublicHeader.h>
+
+
